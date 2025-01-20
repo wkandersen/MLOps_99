907bc62c7c467ab62cfe513ad587a7b719237060
Kirk <s224225@student.dtu.dk>
2025-01-20 15:46:31 +0100

    fix github actions

--- tests/test_data.py
+++ tests/test_data.py
@@ -6,14 +6,14 @@ from torch.utils.data import DataLoader
 from src.group_99.data import load_data, CustomDataset
 import yaml
 
-
+tmp_path = r"${HOME}"
 
 @pytest.fixture
 def mock_kagglehub(tmp_path):
     """
     Mock the kagglehub.dataset_download function and create a fake directory structure.
     """
-    fake_dataset_path = "${HOME}/.cache/kagglehub/datasets/vencerlanz09/sea-animals-image-dataste/versions/5"
+    fake_dataset_path = f"{tmp_path}/.cache/kagglehub/datasets/vencerlanz09/sea-animals-image-dataste/versions/5"
     os.makedirs(os.path.join(fake_dataset_path, "class1"), exist_ok=True)
     os.makedirs(os.path.join(fake_dataset_path, "class2"), exist_ok=True)
 
@@ -95,5 +95,4 @@ def test_data_load_exceptions(mock_path_exists, mock_kagglehub_download):
 
 # Run the tests 1 and 2
 if __name__ == "__main__":
-    print(get_dataset_path_from_config())
-
+    pytest.main(["-v", __file__])
